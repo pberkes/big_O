317edaae15e91b626c56a694856b1ee8639b4a0d
Pietro Berkes <pietro.berkes@googlemail.com>
2011-01-22 11:39:45 -0500

    NEW: setup.py ready

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='big_O',
+      version='0.1',
+      description='Empirical estimation of time complexity from execution time',
+      author='Pietro Berkes',
+      author_email='pietro.berkes@googlemail.com',
+      url='https://github.com/pberkes/big_O',
+      license='LICENSE.txt',
+      description='Useful towel-related stuff.',
+      long_description=open('README.txt').read(),
+      py_modules=['big_o']
+      )
