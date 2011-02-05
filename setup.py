d1a2d8980c038759082d4d987389efd31efc670b
Pietro Berkes <pietro.berkes@googlemail.com>
2011-02-05 12:42:16 -0500

    FIX: setup instructions

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='big_O',
-      version='0.1',
+      version='0.7',
       description='Empirical estimation of time complexity from execution time',
       author='Pietro Berkes',
       author_email='pietro.berkes@googlemail.com',
       url='https://github.com/pberkes/big_O',
       license='LICENSE.txt',
       long_description=open('README.rst').read(),
-      py_modules=['big_o']
+      packages=['big_o', 'big_o.test']
       )
