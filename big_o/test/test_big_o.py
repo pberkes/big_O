9ee9f34adae2b89ad34010927af53ccfe6fbcc70
Pietro Berkes <pietro.berkes@googlemail.com>
2011-02-06 06:44:51 -0500

    FIX: made tests more robuts

--- big_o/test/test_big_o.py
+++ big_o/test/test_big_o.py
@@ -14,13 +14,13 @@ from big_o import complexities as compl
 class TestBigO(unittest.TestCase):
     def test_measure_execution_time(self):
         def f(n):
-            time.sleep(0.01 * n)
+            time.sleep(0.1 * n)
             return n
         ns, t =  big_o.measure_execution_time(f, datagen.n_,
-                                              min_n=1, max_n=10, n_measures=10,
+                                              min_n=1, max_n=5, n_measures=5,
                                               n_repeats=1)
-        assert_array_equal(ns, np.arange(1, 11))
-        assert_array_almost_equal(t*100., np.arange(1, 11), 1)
+        assert_array_equal(ns, np.arange(1, 6))
+        assert_array_almost_equal(t*10., np.arange(1, 6), 1)
 
     def test_infer_big_o(self):
         x = np.linspace(10, 100, 100)
@@ -40,15 +40,15 @@ class TestBigO(unittest.TestCase):
             assert_array_almost_equal(coeff, res_class.coeff, 2)
 
     def test_big_o(self):
-        desired = [(lambda n: [i for i in range(n*1000)], compl.Linear),
+        desired = [(lambda n: [i for i in xrange(n*100)], compl.Linear),
                    (lambda n: 1., compl.Constant),
-                   (lambda n: [i+j for i in range(n) for j in range(n)],
+                   (lambda n: [i+j for i in xrange(n) for j in xrange(n)],
                         compl.Quadratic),
                    (lambda n: sorted(np.random.randn(n*100)),
                         compl.Linearithmic)]
         for func, class_ in desired:
             res_class, fitted = big_o.big_o(func, datagen.n_,
-                                               min_n=100, max_n=1000)
+                                            min_n=100, max_n=1000, n_repeats=5)
             self.assertEqual(class_, res_class.__class__)
 
     def test_compute(self):
