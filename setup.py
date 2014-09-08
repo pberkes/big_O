265c0f54385ada58c06ff75fbc31b163a4f134a7
Simeon Visser <svisser@users.noreply.github.com>
2014-09-08 03:08:40 +0100

    Ensure file handle is closed using with statement

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 
+with open('README.rst') as readme_file:
+      long_description = readme_file.read()
+
 setup(name='big_O',
       version='0.7',
       description='Empirical estimation of time complexity from execution time',
@@ -7,6 +10,6 @@ setup(name='big_O',
       author_email='pietro.berkes@googlemail.com',
       url='https://github.com/pberkes/big_O',
       license='LICENSE.txt',
-      long_description=open('README.rst').read(),
+      long_description=long_description,
       packages=['big_o', 'big_o.test']
       )
