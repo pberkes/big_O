82ae5e5cf3da57af771aa688ec7d951879423578
TheMatt2 <TheMatt2@users.noreply.github.com>
2022-07-26 21:28:05 -0400

    Add compute test cases for all complexity classes

--- big_o/test/test_complexities.py
+++ big_o/test/test_complexities.py
@@ -8,11 +8,23 @@ from big_o import complexities
 class TestComplexities(unittest.TestCase):
 
     def test_compute(self):
+        desired = [
+            (lambda x: 2.+x*0., complexities.Constant),
+            (lambda x: 5.*x+3., complexities.Linear),
+            (lambda x: 8.1*x**2.+0.9, complexities.Quadratic),
+            (lambda x: 1.0*x**3+11.0, complexities.Cubic),
+            (lambda x: 5.2*x**2.5, complexities.Polynomial),
+            (lambda x: 8.5*np.log(x)+99.0, complexities.Logarithmic),
+            (lambda x: 1.7*x*np.log(x)+2.74, complexities.Linearithmic),
+            (lambda x: 3.14**x, complexities.Exponential)
+        ]
+
         x = np.linspace(10, 100, 100)
-        y = 3.0 * x + 2.0
-        linear = complexities.Linear()
-        linear.fit(x, y)
-        assert_array_almost_equal(linear.compute(x), y, 10)
+        for f, class_ in desired:
+            y = f(x)
+            complexity = class_()
+            complexity.fit(x, y)
+            assert_array_almost_equal(complexity.compute(x), y, 10, "compute() failed to match expected values for class %r" % class_)
 
     def test_not_fitted(self):
         linear = complexities.Linear()
