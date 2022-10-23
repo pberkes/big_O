5ce47b102a636a486d49c93851527c0c86bfe888
Harisai <harisaipravin@gmail.com>
2022-10-23 15:21:34 +0530

    FEAT: Function link

--- big_o/__init__.py
+++ big_o/__init__.py
@@ -4,6 +4,7 @@ from big_o.big_o import (  # noqa
     big_o,
     infer_big_o_class,
     measure_execution_time,
+    print_report
 )
 from big_o import complexities  # noqa
 from big_o import datagen  # noqa
