42e0be60111696863a15c1c04aca2d715090769a
Harisai <harisaipravin@gmail.com>
2022-10-28 15:38:15 +0530

    PATCH: Easing Test cases

--- big_o/test/test_reports.py
+++ big_o/test/test_reports.py
@@ -2,19 +2,10 @@ import unittest
 import big_o
 from big_o import reports
 
-def find_max(x):
-    """Find the maximum element in a list of positive integers."""
-    max_ = 0
-    for el in x:
-        if el > max_:
-            max_ = el
-    return max_
-
 class TestReport(unittest.TestCase):
 
     def test_report_return(self):
-        positive_int_generator = lambda n: big_o.datagen.integers(n, 0, 10000)
-        best, others = big_o.big_o(find_max, positive_int_generator, n_repeats=100, return_raw_data=True)
+        best, others = big_o.big_o(sorted, lambda n: big_o.datagen.integers(n, 100, 500))
 
-        assert type(reports.big_o_report(best, others)) == str
+        assert isinstance(reports.big_o_report(best, others), str)
 
