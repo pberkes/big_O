f9839a7c3ca9dac86b47b9ff09475596cdb2fd0c
Harisai <harisaipravin@gmail.com>
2022-10-28 13:15:13 +0530

    DOC: Function Comments

--- big_o/reports.py
+++ big_o/reports.py
@@ -1,6 +1,21 @@
 from big_o.complexities import ALL_CLASSES
 
 def big_o_report(best, others):
+    """ Creates a human-readable report of the output of the big_o function.
+
+    Input:
+    ------
+
+    best -- Object representing the complexity class that best fits
+            the measured execution times.
+    others -- A dictionary of fitted complexity classes to the residuals
+
+    Output:
+    -------
+
+    report -- A string describing the report of the big_o function
+
+    """
     report = ""
     report += 'Best : {!s:<60s} \n'.format(best)
     for class_, residuals in others.items():
